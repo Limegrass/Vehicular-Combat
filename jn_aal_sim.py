062f1936a50d3165d4ae3cfcb8eb0fcf58902706
Limegrass <jani@davidson.edu>
2017-03-31 03:36:54 -0400

    preliminary portions of Q Learning algorithm, More feature
    Velocity normalized, Perpendicular velocity feature, and preliminary
    outline of Q learning algorithm

--- jn_aal_sim.py
+++ jn_aal_sim.py
@@ -16,7 +16,14 @@ TRACK_INNER_RADIUS_Y = 300.0
 TRACK_OUTER_RADIUS_Y = 350.0
 RADIUS_OVER_INERTIA = 1.03212E-7
 MAX_TORQUE = 500
+TORQUE_INCREMENT = 50
+MAX_DELTA_STEERING_ANGLE = math.pi/2
+STEERING_ANGLE_INCREMENT = math.pi/16
 
+
+
+#One idea to simplify all the code could be to swap x and y s.t we don't have to bother with 4 quadrants and instead deal with 2 hemispheres
+#This is if we began at 0 and also travel ccw
 def distance_travelled(x, y, vx):
     theta = math.atan2(y/TRACK_OUTER_RADIUS_Y, x/TRACK_OUTER_RADIUS_X) 
     if(theta < 0):
@@ -91,6 +98,7 @@ def reward(system):
     return reward
 
 def qVal(weights, features):
+    q = 0
     #Steering Angle
     #Distance from Center/Top/Bot of Track
     #Velocity
@@ -98,16 +106,18 @@ def qVal(weights, features):
     #(Torque(Angular Momentum/Yaw, something))?
     #
     #Use a learning rate 1/t
-    pass
+    return q
 
 def f0_constant(steering_angle, front_wheel_torque, rear_wheel_torque, x, y, vx, vy):
     return 1
 
 '''
-How should I be normalizing these?
+Can I normalize to between -1 and 1?
+Easier for things like v
 '''
 
 def f1_steering_angle(steering_angle, front_wheel_torque, rear_wheel_torque, x, y, vx, vy):
+    #Find the angle it should face for forward movement
     theta = math.atan2(y/TRACK_OUTER_RADIUS_Y, x/TRACK_OUTER_RADIUS_X) 
     if(theta >= 0):
         theta -= math.pi/2
@@ -125,10 +135,22 @@ def f3_rwt(steering_angle, front_wheel_torque, rear_wheel_torque, x, y, vx, vy):
 
 def f4_vx(steering_angle, front_wheel_torque, rear_wheel_torque, x, y, vx, vy):
     #Max possible vx that would not crash into a wall 
-    return vx
-
-def f4_vy(steering_angle, front_wheel_torque, rear_wheel_torque, x, y, vx, vy):
-    return vy
+    theta = math.atan2(y/TRACK_OUTER_RADIUS_Y, x/TRACK_OUTER_RADIUS_X) 
+    if(theta >= 0):
+        theta -= math.pi/2
+    else:
+        theta += math.pi/2
+    #Return the difference between the expected fractional component of the current angle
+    # and the current steering angle, divided by the expected
+    # Possibly add a factor of 550/350 ?
+    return abs((vx/(vx**2 + vy**2)**.5 - math.cos(theta)) / math.cos(theta))
+def f5_vy(steering_angle, front_wheel_torque, rear_wheel_torque, x, y, vx, vy):
+    theta = math.atan2(y/TRACK_OUTER_RADIUS_Y, x/TRACK_OUTER_RADIUS_X) 
+    if(theta >= 0):
+        theta -= math.pi/2
+    else:
+        theta += math.pi/2
+    return abs((vy/(vx**2 + vy**2)**.5 - math.sin(theta)) / math.sin(theta))
 
 #def least_squares(weight, change)
 
@@ -137,14 +159,20 @@ Larger than the distance to the inner/outer wall would lead to a wall assuming a
 Ratio of vx or vy vs dist-wall-x and dist-wall-y
 Not sure if it's a useful parameter since we already have vy&vx
 '''
-def f5_highVx(steering_angle, front_wheel_torque, rear_wheel_torque, lat_vel, long_vel):
-    return true
-#def f6_vy equiv
+def f6_high_vx(steering_angle, front_wheel_torque, rear_wheel_torque, x, y, vx, vy): 
+    theta = math.atan2(y/TRACK_OUTER_RADIUS_Y, x/TRACK_OUTER_RADIUS_X) 
+    if(theta >= 0):
+        theta -= math.pi/2
+    else:
+        theta += math.pi/2
+    #If the tangential velocity to a wall relative to the direction the car should face is >50
+    return abs(vx*math.cos(theta) - vy * math.sin(theta))/50
 
 #theta as a function of velocity might be useful since going too fast limits turning
 #def f7_thetaV
 
 def oursim():
+    
     
     DISCOUNT_FACTOR = 1
     
@@ -156,30 +184,41 @@ def oursim():
     time = 1
     learning_rate= 1
     weights= []
+    features = [f0_constant, f1_steering_angle, f2_fwt, f3_rwt, f4_vx, f5_vy, f6_high_vx ]
+    for i in range(7):
+        weights[i] = uniform(0, 5)
     #Initialize random weights
     #Define a function fn and put in array so we can call w_n f_n
       
     for i in range(10000):
         
         cur_model = VehicleTrackSystem()
+        #should it be time = i
         time = 1
         learning_rate = 1/time
-        angle = -.5
-        best_angle = 0
-        best_q_val = 0
        
-        while True:
+        while cur_model.is_on_track:
             #Actually, we should do a +- range from current steering angle so we don't hard steer 
             #Also for a range of torques
-            while angle < 0.51:
-                #thing with angle
-                system.tick_simulation(front_wheel_torque=front_wheel_torque,
-                                               rear_wheel_torque=rear_wheel_torque,
-                                               steering_angle=steering_angle)                
-                
-                angle += .05
-                
+            best_q_val = 0
+            best_torque = 0
+            best_angle = steering_angle 
+            for test_torque in range(-500, 500):
+                test_angle = steering_angle - math.pi/2
+                while test_angle <= steering_angle + math.pi/2:
+                    #thing with angle
+                    test_angle += STEERING_ANGLE_INCREMENT
+                    #Test parameters
+                    #Calculate predicted Q vals
+                    #Save best angle and torque
+                    
+                    
+            #Take best choice and use the simualtion on it
+            system.tick_simulation(front_wheel_torque=best_torque,
+                                           rear_wheel_torque=best_torque,
+                                           steering_angle=best_angle)                      
             time+=1
+            learning_rate = 1/time
         #Use history and create new weights
         #Use old weight values and new points to
         #Modify our new weight values
