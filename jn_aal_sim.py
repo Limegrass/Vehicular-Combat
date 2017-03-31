6c08d84092f14cd914d1792f8605314742199e2f
Limegrass <jani@davidson.edu>
2017-03-31 15:18:22 -0400

    Runnable, no weights updating

--- jn_aal_sim.py
+++ jn_aal_sim.py
@@ -2,6 +2,7 @@
 Path Finding using Q-Learning
 Author: JN, AAL
 '''
+import random
 import math
 import numpy
 from random import uniform
@@ -101,7 +102,7 @@ def qVal(weights, features, steering_angle, front_wheel_torque, rear_wheel_torqu
     q = 0
     #Steering Angle
     for i in range(0, 7):
-        q += weights[i]*features(steering_angle, front_wheel_torque, rear_wheel_torque, x, y, vx, vy)
+        q += weights[i]*features[i](steering_angle, front_wheel_torque, rear_wheel_torque, x, y, vx, vy)
     #(Theta(v))?
     #(Torque(Angular Momentum/Yaw, something))?
     return q
@@ -190,50 +191,76 @@ def oursim():
     #Define a function fn and put in array so we can call w_n f_n
       
     #for i in range(10000):
-    for i in range(1):
-        cur_model = VehicleTrackSystem()
-        #i represents time/episode
-        learning_rate = 1/(i+1)
-       
-        while cur_model.is_on_track:
-            #Actually, we should do a +- range from current steering angle so we don't hard steer 
-            #Also for a range of torques
-            best_q_val = CRASH_PUNISHMENT
-            best_torque = 0
-            best_angle = steering_angle 
-            for torque_multiplier in range(-10, 10):
-                test_torque = torque_multiplier*TORQUE_INCREMENT
-                test_angle = steering_angle - math.pi/2
-                while test_angle <= steering_angle + math.pi/2:
-                    predictions = cur_model.predict_states(test_torque, test_torque, test_angle)
-                    test_q = qVal(test_torque, test_torque, test_angle, predictions.x, predictions.y, predictions.vx, predictions.vy)
-                    #thing with angle
-                    test_angle += STEERING_ANGLE_INCREMENT
-                    if test_q > best_q_val:
-                        best_torque = test_torque
-                        best_angle = test_angle
-                    #Test parameters
-                    #Calculate predicted Q vals
-                    #Save best angle and torque
+    for i in range(100):
+        try:
+            cur_model = VehicleTrackSystem()
+            #i represents time/episode
+            learning_rate = 1/(i+1)
+            
+           
+            while cur_model.is_on_track:
+                #Actually, we should do a +- range from current steering angle so we don't hard steer 
+                #Also for a range of torques
+                best_q_val = CRASH_PUNISHMENT
+                best_torque_multiplier = 0
+                best_angle_multplier = 0 
+                for torque_multiplier in range(-10, 11):
+                    test_torque = torque_multiplier*TORQUE_INCREMENT
+                                        for angle_multiplier in range(-8, 9):
+                        
+                        test_angle = steering_angle + (angle_multiplier*STEERING_ANGLE_INCREMENT)
+                        x, y, vx, vy = cur_model.predict_states(test_torque, test_torque, test_angle)
+                        test_q = qVal(weights, features, test_torque, test_torque, test_angle, x, y, vx, vy)
+                        #thing with angle
+                        if test_q > best_q_val:
+                            best_torque_multiplier = torque_multiplier
+                            best_angle_multplier = angle_multiplier
+                        #Test parameters
+                        #Calculate predicted Q vals
+                        #Save best angle and torque
+                        
+                        
+                #print best_angle_multplier
+                #print best_torque_multiplier
+                if uniform(0,1) < learning_rate: 
                     
-                    
-                    
-            #Take best choice and use the simualtion on it
-            system.tick_simulation(front_wheel_torque=best_torque,
-                                           rear_wheel_torque=best_torque,
-                                           steering_angle=best_angle)                      
-        #Use history and create new weights
-        #Use old weight values and new points to
-        #Modify our new weight values
-        #Reassign old weights to new weights for persistence
-        
-        #I just wanted to simulate to pause per step to see the effects
-        #var = raw_input("Give me  the input here: ")
+                  #  print best_angle_multplier
+                   # print best_torque_multiplier
+                    test_different_torque_multiplier = random.randint(-10, 10)
+                    while (test_different_torque_multiplier == best_torque_multiplier):
+                        test_different_torque_multiplier = random.randint(-10, 10)
+                    test_different_angle_multiplier = random.randint(-8, 8)
+                    while(test_different_angle_multiplier == best_angle_multplier):
+                        test_different_angle_multiplier = random.randint(-8, 8)
+                    if(steering_angle+(test_different_angle_multiplier*STEERING_ANGLE_INCREMENT) > math.pi):
+                        steering_angle -= 2*math.pi
+                    elif(steering_angle - (test_different_angle_multiplier * STEERING_ANGLE_INCREMENT) < math.pi):
+                        steering_angle += 2*math.pi
+                    cur_model.tick_simulation(front_wheel_torque=TORQUE_INCREMENT*test_different_torque_multiplier,
+                                                   rear_wheel_torque=TORQUE_INCREMENT*test_different_torque_multiplier,
+                                                   steering_angle=steering_angle+(test_different_angle_multiplier*STEERING_ANGLE_INCREMENT))                       
+                        
+                else:
+                #Take best choice and use the simualtion on it
+                    cur_model.tick_simulation(front_wheel_torque=best_torque,
+                                                   rear_wheel_torque=best_torque,
+                                                   steering_angle=best_angle)                      
+            #Use history and create new weights
+            #Use old weight values and new points to
+            #Modify our new weight values
+            #Reassign old weights to new weights for persistence
+            
+            #I just wanted to simulate to pause per step to see the effects
+        except SimulationError:
+            pass
+            #cur_model.plot_history()
+            
+            #var = raw_input("Give me  the input here when ready to move on: ")
         
     
         #Update weights
         
-        #system.plot_history()
+        cur_model.plot_history()
 
 def main():
     #print dist_from_inner_wall(500*math.cos(0.5*math.pi),300*math.sin(0.5*math.pi))
